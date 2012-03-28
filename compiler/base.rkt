4c844380cafff94a35d399b0e0d4ca14f48535f6
Travis Charlton <charltt@allegheny.edu>
2012-03-28 16:31:33 -0400

    updated base.rkt with structures

--- compiler/base.rkt
+++ compiler/base.rkt
@@ -1,1 +1,20 @@
+<<<<<<< HEAD
 #lang racket
+
+(provide (all-defined-out))
+
+;; Structures
+(struct E (op lhs rhs)
+  #:inspector (make-inspector))
+
+
+(struct id (sym simple)
+  #:inspector (make-inspector))
+
+
+
+;; NOT YET COMPLETED, MORE STRUCTURES MUST BE ADDED
+;; AS THE PROJECT CONTINUES AND STRUCTURES ARE NEEDED
+=======
+#lang racket
+>>>>>>> 7c3059b7ee1cb1b4e046b51aab7353753b6fdc3a
