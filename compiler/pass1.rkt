728d7b269c4fe1cf222b831d63b2abca2bb2f4e3
Trent Dale <dalet@allegheny.edu>
2012-03-31 14:37:40 -0400

    Finished up the pass that adds simple structs.

--- compiler/pass1.rkt
+++ compiler/pass1.rkt
@@ -10,12 +10,14 @@
 ;; PURPOSE
 ;; Take in an expression and produce num and binop structs
 ;; CONTRACT
-;; pass1.1 : exp -> struct
+;; pass-to-struct : exp -> struct
 (define (pass-to-struct e)
   (cond
     [(empty? (first e)) '()]
     [(number? (first e)) (num (first e)) (pass-to-struct (rest e))]
-    [(symbol? (first e)) (binop (first e) (pass-to-struct (rest e)) (pass-to-struct (rest e)))]
+    [(symbol? (first e)) (binop (first e) 
+                                (pass-to-struct (rest e)) 
+                                (pass-to-struct (rest e)))]
     )
   )
 
@@ -24,10 +26,14 @@
 
 
 ;; PURPOSE
-;; Take in the structures of pass 1.1 and produce structs with simple structs
+;; Take in the structures of pass-to-struct and produce structs with simple structs
 ;; CONTRACT
-;; pass1.2 : struct -> struct with simples
+;; pass-add-simple : struct -> struct with simples
 (define (pass-add-simple e)
   (cond
     [(empty? (first e)) '()]
-    [
+    [(num? (first e)) (num (first e)) (pass-add-simple (rest e))]
+    [(binop? (first e)) (simple (first e) 
+                                (pass-add-simple (rest e)) 
+                                (pass-add-simple (rest e)))]
+    ))
