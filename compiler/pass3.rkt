b2998251ad279fa5767161a075e1ca32ae4e6950
Braden Licastro <licastb@allehgeny.edu>
2012-04-03 10:48:24 -0400

    Added in a function for ending the program and partially implemented the simple pass.

--- compiler/pass3.rkt
+++ compiler/pass3.rkt
@@ -16,13 +16,28 @@
      (string-append (asm-num (first input)) (pass-to-asm (rest input)))
      ]
     [(simple? (id-value input))
-     ]
-    ))
+     (string-append (asm-simple (first input)) (pass-to-asm (rest input)))
+     ])
+  )
 
 ; CONTRACT
-;; input ID -> String
+;; input Num ID -> String
 (define (asm-num input)
   (string-append "@" (id-value input) "\n"
                  "D=A\n"
                  "@" (id-sym input) "\n"
                  "M=D\n"))
+
+; CONTRACT
+;; input Sym ID -> String
+(define (asm-sym input)
+  (string-append "@" (simple-lhs (id-value input)) "\n"
+                 (cond
+                   [(
+                 "D=A\n"
+                 "@"
+                 
+; CONTRACT
+;; input Nothing -> String
+(define (prog-end)
+  )
