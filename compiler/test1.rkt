306c92c980cf11de710bb920f6cf8ae6f1df8440
fodym <fodym@allegheny.edu>
2012-04-03 10:48:44 -0400

    changed a few things in test1

--- compiler/test1.rkt
+++ compiler/test1.rkt
@@ -8,20 +8,21 @@
 
 (check-equal? (pass-to-struct 3) (num 3))
 (check-equal? (pass-to-struct '(+ 3 5)) (binop '+ (num 3) (num 5)))
-
-(check-equal? (pass-to-struct '(- 3
-                        (+ (- 5 
-                              (+ 2 7))
-                           (+ (- (- 2 3) 3)              
-                              (+ 4 (+ 3 (- 4 2)))))))
+(check-equal? (pass-to-struct '(+ (- 5 2) (+ 3 2))) (binop '+ 
+                                                           (binop '- (num 5) (num 2))
+                                                           (binop '+ (num 3) (num 2))))
+;;nuke test
+(check-equal? (pass-to-struct '(- 3(+ (- 5 (+ 2 7))
+                                      (+ (- (- 2 3) 3)              
+                                         (+ 4 (+ 3 (- 4 2)))))))
               
               (binop '- (num 3)
                         (binop '+ 
                                (binop '- (num 5) 
-                                         (binop + (num 2) (num 7)))
+                                         (binop '+ (num 2) (num 7)))
                                (binop '+ (binop '- (binop '- (num 2)(num 3))
                                                    (num 3))
-                                         (binop + (num 4)
+                                         (binop '+ (num 4)
                                                   (binop '+ (num 3)
                                                             (binop '- (num 4) 
                                                                       (num 2))))))))
