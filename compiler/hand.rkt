6e828dbbf7e23164d6f84b3a0b8d6fc5ddc9e0bd
Molly Mattis <molly.mattis@gmail.com>
2012-04-03 10:27:57 -0400

    added few more hand written tests

--- compiler/hand.rkt
+++ compiler/hand.rkt
@@ -16,40 +16,40 @@
  (test-case
   "(+ 3 5)"
   (compile '(+ 3 5) "foo.hack")
-  (check-files "foo.hack" "testingFiles/one.hack")
+  (check-files "foo.hack" "testingFiles/one.hack"))
 (test-case
  "(- 9 1)"
  (compile '(- 9 1) "foo.hack")
- (check-files "foo.hack" "testingFiles/two.hack")
+ (check-files "foo.hack" "testingFiles/two.hack"))
 (test-case
  "(+ 3(- 5 2)"
  (compile '(+ 3(- 5 2)) "foo.hack")
- (check-files "foo.hack" "testingFiles/three.hack")
+ (check-files "foo.hack" "testingFiles/three.hack"))
 (test-case
  "(- 3(+ 5 2)"
  (compile '(- 3(+ 5 2)) "foo.hack")
- (check-files "foo.hack" "testingFiles/four.hack")
+ (check-files "foo.hack" "testingFiles/four.hack"))
 (test-case
  "(- 3(+ 5 2)"
  (compile '(- 3(+ 5 2)) "foo.hack")
- (check-files "foo.hack" "testingFiles/four.hack")
+ (check-files "foo.hack" "testingFiles/four.hack"))
 (test-case
  "(+(+ 1 2)(- 4 3))"
  (compile '(+(+ 1 2)(- 4 3)) "foo.hack")
- (check-files "foo.hack" "testingFiles/five.hack")
+ (check-files "foo.hack" "testingFiles/five.hack"))
 (test-case
  "(-(- 8 1)(+ 1 3))"
  (compile '(+(+ 1 2)(- 4 3)) "foo.hack")
- (check-files "foo.hack" "testingFiles/six.hack")
+ (check-files "foo.hack" "testingFiles/six.hack"))
 (test-case
  "(+(+ 3 5) 5)"
  (compile '(+(+ 1 2)(- 4 3)) "foo.hack")
- (check-files "foo.hack" "testingFiles/seven.hack")
-
-
-
-(define actual1
-  (file->list "testactual.txt"))
-(define expected1
-  (file->list "testfile.txt" ))
-(check-equal? actual1 expected1 "is this assembly as expected?")
+ (check-files "foo.hack" "testingFiles/seven.hack"))
+(test-case
+ "(+(- 3 1) 6))"
+ (compile '(+(- 3 1)6) "foo.hack")
+ (check-files "foo.hack" "testingFiles/eight.hack"))
+(test-case
+ "(-(- 4 2)1)"
+ (compile '(-(-4 2)1) "foo.hack")
+ (check-files "foo.hack" "testingFiles/nine.hack")))
