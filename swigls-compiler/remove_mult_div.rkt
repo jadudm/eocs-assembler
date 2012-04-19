88089bc3cfaa9ef0f4fb6a6fb0251be05b8ed8fd
Zachary Williams <williaz@aldenv157.allegheny.edu>
2012-04-19 10:57:25 -0400

    removed div

--- swigls-compiler/remove_mult_div.rkt
+++ swigls-compiler/remove_mult_div.rkt
@@ -5,65 +5,44 @@
 ;;CONTRACT
 ;; nested statement -> nested statment (without * and /)
 ;; replace multiplication and division with while loops
-(define (remove-mult-div statement)
+(define (remove-mult statement)
   (cond
     [(binop? statement)
      (cond
-       [(equal? statement-op '*)
+       [(equal? (binop-op statement) '*)
         (let ([flag (gensym 'flag)])
           (let ([inc (gensym 'inc)])
             (let ([val (gensym 'val)]) 
-              (remove-mul-div 
+              (remove-mult 
                (seq 
                 (list
                 (set flag 0)
                 (set val 0)
-                (set inc (remove-mult-div statement-lhs))
+                (set inc (remove-mult (binop-lhs statement)))
                 (while0 flag
                         (seq
                          (if0 inc
                               (set flag 1)
                               (set inc (binop '- inc 1)))
-                         (set val (binop '+ val (remove-mult-div statement-rhs)))))
+                         (set val (binop '+ val (remove-mult (binop-rhs statement))))))
                 ; need to somehow return value
-                val))))))]   
-       [(equal? statement-op '/)
-        (let ([flag (gensym 'flag)])
-          (let ([inc (gensym 'inc)])
-            (let ([val (gensym 'val)])
-              (let ([count (gensym 'count)]) 
-              (remove-mul-div 
-               (seq 
-                (list
-                 (set flag 0)
-                 (set val 0)
-                 (set inc (remove-mult-div stement-lhs))
-                 (while0 flag
-                         (seq
-                          (if0 inc
-                               (set flag 1)
-                               (set inc (binop '- inc 1)))
-                          (set val (binop '+ val (remove-mult-div stement-rhs)))))
-                 ; need to somehow return value
-                count)))))))]
+                val))))))]    
        [else
-        (binop statement-op 
-               (remove-mul-div statement-lhs)
-               (remove-mul-div statement-lhs))]
+        (binop (binop-op statement) 
+               (remove-mult (binop-lhs statement))
+               (remove-mult (binop-rhs statement)))]
        )]
 
        
-    [(if0? statement) (if0 (remove-mul-div statement-test) 
-                          (remove-mul-div statement-truecase) 
-                          (remove-mul-div statement-falsecase))]
-    [(while0? statement) (while0  (remove-mul-div statment-test) 
-                                  (remove-mul-div statment-body))]
-    [(seq? statement) (seq (map remove-mul-div statment-expressions))]
-    [(set? statement) (set statment-ident (remove-mul-div statment-e))]
-    [(num? statement) (num statement-value)]
-    [(symbol? statement) (symbol statement-value)]
-
-    
+    [(if0? statement) (if0 (remove-mult (if0-test statement)) 
+                          (remove-mult (if0-truecase statement)) 
+                          (remove-mult(if0-falsecase statement)))]
+    [(while0? statement) (while0  (remove-mult (while0-test statement)) 
+                                  (remove-mult (while0-body statement)))]
+    [(seq? statement) (seq (map remove-mult (seq-expressions statement)))]
+    [(set? statement) (set (set-ident statement) (remove-mult (set-e statement)))]
+    [(num? statement) (num (num-value statement))]
+    [(symbol? statement) (symbol (symbol-value statement))]   
   ))
 
 
